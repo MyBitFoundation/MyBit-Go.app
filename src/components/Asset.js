960b5061d7b7f20127a1c751d50d1250377d1180
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-04-23 09:45:01 +0100

    Feature/explore assets page (#3)
    * Initial commit
    
    * Initial commit
    
    * Initial commit
    
    * Responsive page and a few more things
    
    * Fixed no-unused-vars and added alt prop to image
    
    * Switched to pascal casing

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,3 +1,63 @@
 import React from 'react';
+import '../styles/Asset.css';
+import { Button } from 'carbon-components-react';
+import locationIcon from '../images/Location-icon.svg';
+import { debug } from '../constants';
 
-export const Asset = () => <div />;
+export const Asset = ({
+  clickHandler,
+  funded,
+  goal,
+  image,
+  path,
+  city,
+  country,
+  name
+}) => {
+  let barWidth = Math.ceil(100 * funded / goal) + '%';
+  return (
+    <div className="col-3_md-4_sm-6_xs-12 Asset">
+      <div className="Asset__container">
+        <div
+          className="Asset__image-holder"
+          style={{ backgroundImage: 'url(' + image + ')' }}
+        >
+          <div className="Asset__image-holder-gradient" />
+          <img
+            alt="Location icon"
+            className="Asset__image-holder-location-icon"
+            src={locationIcon}
+          />
+          <b className="Asset__image-holder-name">{name}</b>
+          <p className="Asset__image-holder-location">
+            {city}, <span>{country}</span>
+          </p>
+        </div>
+        <div className="Asset__details">
+          <p className="Asset__details-funded">
+            Funded: <b>${funded}</b>
+          </p>
+          <p className="Asset__details-goal">
+            Goal: <b>${goal}</b>
+          </p>
+          <div className="Asset__details-progress-bar">
+            <div
+              className="Asset__details-progress-bar-fill"
+              style={{ width: barWidth }}
+            />
+          </div>
+          <Button
+            onClick={
+              clickHandler
+                ? clickHandler
+                : () => debug(`Clicked to contribute, path: ${path}`)
+            }
+            className="Asset__details-contribute"
+          >
+            CONTRIBUTE
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/ExploreAssetsPage.js
+++ src/components/ExploreAssetsPage.js
@@ -1,3 +1,67 @@
 import React from 'react';
+import '../styles/ExploreAssetsPage.css';
+import { Asset } from './Asset';
+import { Button, Loading } from 'carbon-components-react';
+import { debug } from '../constants';
 
-export const ExploreAssetsPage = () => <div />;
+export const ExploreAssetsPage = ({
+  clickHandler,
+  loading,
+  assetsInfo,
+  category
+}) => {
+  const backButton = (
+    <Button
+      kind="secondary"
+      className="ExploreAssetsPage__back-button"
+      onClick={debug('Clicked to go back')}
+    >
+      BACK
+    </Button>
+  );
+
+  const assets = loading
+    ? null
+    : assetsInfo.length === 0
+      ? null
+      : [
+          backButton,
+          assetsInfo.map(asset => {
+            return (
+              <Asset
+                key={asset.path}
+                clickHandler={clickHandler}
+                funded={asset.funded}
+                goal={asset.goal}
+                image={asset.image}
+                path={asset.path}
+                city={asset.city}
+                country={asset.country}
+                name={asset.name}
+              />
+            );
+          })
+        ];
+
+  const loadingElement = loading && (
+    <div style={{ width: '100%' }}>
+      {backButton}
+      <Loading className="ExploreAssetsPage--is-loading" withOverlay={false} />
+      <p className="ExploreAssetsPage-loading-message">Loading assets</p>
+    </div>
+  );
+
+  const noElements =
+    !loading && assetsInfo.length === 0 ? (
+      <div style={{ width: '100%' }}>
+        {backButton}
+        <p className="ExploreAssetsPage__message-no-elements">{`No assets found in the ${category} category.`}</p>
+      </div>
+    ) : null;
+
+  const toRender = loading
+    ? loadingElement
+    : assetsInfo.length === 0 ? noElements : assets;
+
+  return <div className="ExploreAssetsPage grid">{toRender}</div>;
+};
