1c2705bdd40f102eecb7d105a04f488914f8e9f6
connorblockchain <connorblockchain@protonmail.com>
2018-04-27 09:58:52 +0200

    Adding asset total required funding

--- src/components/AssetFunding.js
+++ src/components/AssetFunding.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { promisifyAll } from 'bluebird';
+
+/* --- First way ---*/
+import { default as DatabaseUtil } from './contracts/DatabaseUtil';
+import { default as HashFunctionsUtil } from './contracts/HashFunctionsUtil';
+
+/* --- Second Way way ---*/
 import {
-  ASSET_ABI_INTERFACE,
-  ASSET_CONTRACT_ADDRESS
-} from './contracts/HelloWorld.js';
+  DATABASE_CONTRACT_ADDRESS,
+  DATABASE_ABI_INTERFACE
+} from './contracts/Database.js';
 
 const instancePromisifier = instance =>
   promisifyAll(instance, { suffix: 'Async' });
@@ -12,22 +18,43 @@ class AssetFunding extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      instance: null,
-      hello: null
+      hashFunctionsInstance: null,
+      databaseInstance: null,
+      amountToBeRaised: null,
+      assetID: null
     };
   }
   async componentDidMount() {
     const { web3 } = this.props || {};
-    const abi = await web3.eth.contract(ASSET_ABI_INTERFACE);
-    const instance = instancePromisifier(abi.at(ASSET_CONTRACT_ADDRESS));
-    this.setState({
-      instance: instance,
-      hello: await instance.helloRawAsync()
-    });
+    if (web3.isConnected()) {
+      const assetID =
+        '0xcbb8744ce8cb288674fbafe1b6c111c6d86308af6b2e7d396780104341a1cf8e';
+      const hashFunctionsInstance = new HashFunctionsUtil();
+      const databaseInstance = new DatabaseUtil();
+      await hashFunctionsInstance.load(web3);
+      await databaseInstance.load(web3);
+
+      this.setState({
+        hashFunctionsInstance: hashFunctionsInstance,
+        databaseInstance: databaseInstance,
+        amountToBeRaised: web3.fromWei(
+          await databaseInstance.uintStored(
+            await hashFunctionsInstance.stringBytes('amountToBeRaised', assetID)
+          ),
+          'ether'
+        )
+      });
+
+      console.log('amountToBeRaised', amountToBeRaised);
+    }
   }
 
   render() {
-    return <div className="AssetFunding">Asset Funding {this.state.hello}</div>;
+    return (
+      <div className="AssetFunding">
+        Asset Funding {this.state.amountToBeRaised}
+      </div>
+    );
   }
 }
 
