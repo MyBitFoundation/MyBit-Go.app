629ea39b1ee2be843a686e29e81580d0da8c9b03
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-02 16:56:10 +0100

    Fixed linting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import 'carbon-components/css/carbon-components.min.css';
+import 'gridlex/dist/gridlex.min.css';
 import './styles/index.css';
 import './styles/semantic-custom.css';
-import 'gridlex/dist/gridlex.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { connect, Provider } from 'react-redux';
-import { BrowserRouter, withRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
