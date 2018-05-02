471648d4c904087cc98ed39379855a0ae527a9e6
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-02 16:49:04 +0100

    Quick test update

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
+// TODO: Use enzyme + shallow rendering
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>,
     div
   );
