3a1e56ac9f434c50dd2b554e358c75b8f2730272
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-05-03 18:57:20 +0100

    Fixed test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import configureStore from './store/configureStore';
+import { BrowserRouter } from 'react-router-dom';
+
+const store = configureStore();
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = shallow(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
   const expected = 1;
   const actual = wrapper.length;
   expect(actual).toEqual(expected);
