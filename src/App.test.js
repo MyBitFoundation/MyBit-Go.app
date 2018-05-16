8416dc3503426008db5d5d8aaa6903ae42dcad37
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-16 17:23:19 +0100

    Unit test is now stateless

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import configureStore from './store/configureStore';
-
-const store = configureStore();
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const toRender = (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  );
+  const toRender = <App />;
   const wrapper = shallow(toRender);
   const expected = 1;
   const actual = wrapper.length;
