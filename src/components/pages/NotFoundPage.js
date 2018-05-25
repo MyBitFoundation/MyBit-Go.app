dcf2c7e7655afde11af4536677d28c466a2bad05
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-05-25 09:13:07 +0100

    not found page

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import NotFound from '../../images/404.svg';
+import '../../styles/NotFoundPage.css';
 
-const NotFoundPage = () => (<div><h1>Not Found - Sorry!</h1></div>);
+const NotFoundPage = () => (
+  <div className="NotFoundPage">
+    <img className="NotFoundPage__image" alt="Page not found" src={NotFound} />
+    <h1 className="NotFoundPage__header-text">Page not found.</h1>
+    <h2 className="NotFoundPage__message-text">The page you‘re looking for doesn‘t exist.</h2>
+    <div className="NotFoundPage__background-circle-right" />
+    <div className="NotFoundPage__background-circle-left" />
+  </div>
+);
 
 export default NotFoundPage;
