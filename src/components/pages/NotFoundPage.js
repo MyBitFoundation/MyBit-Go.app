02eb0d4243acf25ddcd0388bf18fad5f3f0e9d47
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-05-25 10:29:41 +0100

    Added button to go back to not found page

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button } from 'carbon-components-react';
+import { Link } from 'react-router-dom';
+
 import NotFound from '../../images/404.svg';
 import '../../styles/NotFoundPage.css';
 
@@ -9,6 +12,13 @@ const NotFoundPage = () => (
     <h2 className="NotFoundPage__message-text">The page you‘re looking for doesn‘t exist.</h2>
     <div className="NotFoundPage__background-circle-right" />
     <div className="NotFoundPage__background-circle-left" />
+    <Link to="/explore" href="/explore" className="NotFoundPage__home-btn">
+      <Button
+        kind="secondary"
+      >
+          GO TO HOME
+      </Button>
+    </Link>
   </div>
 );
 
