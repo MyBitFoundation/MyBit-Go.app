80dcc0695d0456f6f15b19926362f70e30ffc0b5
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-25 21:37:49 +0100

    Fetch data before removing comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 // TODO: The previously suppressed error can actually be avoided with better syntax
 import getWeb3Async from '../util/web3';
+import * as API from '../constants/contracts/API';
 import * as AssetCreation from '../constants/contracts/AssetCreation';
 import * as FundingHub from '../constants/contracts/FundingHub';
 import { MYBIT_TICKER_COINMARKETCAP, ETHEREUM_TICKER_COINMARKETCAP } from '../constants';
 
-import { mergeAllLogsByAssetId, mergeAndSumFundingEvents } from '../util/helpers';
 import { getCategoryFromAssetTypeHash, mergeAllLogsByAssetId, mergeAndSumFundingEvents } from '../util/helpers';
 
 const web3 = getWeb3Async();
@@ -49,8 +49,8 @@ export const fetchPriceFromCoinmarketcap = ticker => async (dispatch) => {
   }
   try {
     const response = await fetch(`https://api.coinmarketcap.com/v2/ticker/${ticker}/`);
-    const jsonResponse = response.json();
-    const { price } = jsonResponse.data.data.quotes.USD;
+    const jsonResponse = await response.json();
+    const { price } = jsonResponse.data.quotes.USD;
     switch (ticker) {
       case MYBIT_TICKER_COINMARKETCAP:
         dispatch({ type: FETCH_MYBIT_PRICE_USD_SUCCESS, payload: { price } });
@@ -78,17 +78,18 @@ export const fetchPriceFromCoinmarketcap = ticker => async (dispatch) => {
 export const fetchAssets = () => async (dispatch, getState) => {
   dispatch(clearErrors());
   dispatch({ type: FETCH_ASSETS });
+  dispatch(fetchPriceFromCoinmarketcap(ETHEREUM_TICKER_COINMARKETCAP));
   try {
-    getState();
+    const apiContract = new web3.eth.Contract(API.ABI, API.ADDRESS);
     const assetCreationContract = new web3.eth.Contract(AssetCreation.ABI, AssetCreation.ADDRESS);
-    const fundingHubContract = new web3.eth.Contract(FundingHub.ABI, FundingHub.ADDRESS);
+    // const fundingHubContract = new web3.eth.Contract(FundingHub.ABI, FundingHub.ADDRESS);
 
     const logAssetInfoEvents =
       await assetCreationContract
         .getPastEvents('LogAssetInfo', { fromBlock: 0, toBlock: 'latest' });
-    const logAssetFundingSuccessEvents =
-      await fundingHubContract
-        .getPastEvents('LogAssetFundingSuccess', { fromBlock: 0, toBlock: 'latest' });
+    // const logAssetFundingSuccessEvents =
+    //   await fundingHubContract
+    //     .getPastEvents('LogAssetFundingSuccess', { fromBlock: 0, toBlock: 'latest' });
     const logAssetFundingStartedEvents =
       await assetCreationContract
         .getPastEvents('LogAssetFundingStarted', { fromBlock: 0, toBlock: 'latest' });
@@ -100,18 +101,18 @@ export const fetchAssets = () => async (dispatch, getState) => {
         installerID: object._installerID,
         amountToBeRaised: object._amountToBeRaised,
       }));
-    const logAssetFundingSuccess = logAssetFundingSuccessEvents
-      .map(({ returnValues }) => returnValues)
-      .map(object => ({
-        assetID: object._assetID,
-        currentEthPrice: object._currentEthPrice,
-        timestamp: object._timestamp,
-      }))
-      .sort((a, b) => {
-        if (a.assetID < b.assetID) { return -1; }
-        if (a.assetID > b.assetID) { return 1; }
-        return 0;
-      });
+    // const logAssetFundingSuccess = logAssetFundingSuccessEvents
+    //   .map(({ returnValues }) => returnValues)
+    //   .map(object => ({
+    //     assetID: object._assetID,
+    //     currentEthPrice: object._currentEthPrice,
+    //     timestamp: object._timestamp,
+    //   }))
+    //   .sort((a, b) => {
+    //     if (a.assetID < b.assetID) { return -1; }
+    //     if (a.assetID > b.assetID) { return 1; }
+    //     return 0;
+    //   });
     const logAssetFundingStarted = logAssetFundingStartedEvents
       .map(({ returnValues }) => returnValues)
       .map(object => ({
@@ -120,11 +121,11 @@ export const fetchAssets = () => async (dispatch, getState) => {
         creator: object._creator,
       }));
 
-    const talliedLogsAssetFundingSuccess = mergeAndSumFundingEvents(logAssetFundingSuccess);
+    // const talliedLogsAssetFundingSuccess = mergeAndSumFundingEvents(logAssetFundingSuccess);
 
     const combinedLogs =
       logAssetInfo
-        .concat(talliedLogsAssetFundingSuccess)
+        // .concat(logAssetFundingSuccess)
         .concat(logAssetFundingStarted)
         .sort((a, b) => {
           if (a.assetID < b.assetID) { return -1; }
@@ -133,18 +134,29 @@ export const fetchAssets = () => async (dispatch, getState) => {
         });
 
     const assets = mergeAllLogsByAssetId(combinedLogs);
-    dispatch(fetchAssetsSuccess(assets));
+
+    const amountsRaised =
+      await Promise.all(assets.map(async asset => apiContract.methods.amountRaised(asset.assetID)
+        .call()));
+
+    const assetsPlusRaised = assets.map((asset, index) => ({
+      ...asset,
+      amountRaisedInUSD: String(Number(web3.utils.fromWei(amountsRaised[index], 'ether')) * getState().misc.currentEthInUsd),
+    }));
+
+    const assetsWithCategories = assetsPlusRaised.map((asset) => {
+      if (asset.assetType) {
+        return { ...asset, category: getCategoryFromAssetTypeHash(web3, asset.assetType) };
+      }
+      return { ...asset };
+    });
+    dispatch(fetchAssetsSuccess(assetsWithCategories));
   } catch (error) {
     dispatch(fetchAssetsFailure(error));
   }
 };
 
 // TODO: as followed
-
-// Categories from AssetCreation/LogAssetFundingStarted comes from _assetType
-
-// Creator of an asset which comes from AssetCreation/LogAssetFundingStarted
-// from _creator
 
 // Funds raised and funding goals coming from AssetCreation/LogAssetInfo events and
 // FundingHub/LogAssetFundingSuccess (frontend note: more fetchAssets functionality)
