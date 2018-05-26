24fe09a0e775d20eace406d746a441ae1052a2c6
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-26 16:15:09 +0100

    Connected router to redux state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux';
 import 'carbon-components/css/carbon-components.min.css';
 import 'gridlex/dist/gridlex.min.css';
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
+import { fetchAssets } from './actions';
 
 const store = configureStore();
+const history = createHistory();
+
+store.dispatch(fetchAssets());
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <ConnectedRouter history={history}>
       <App />
-    </BrowserRouter>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
 );
