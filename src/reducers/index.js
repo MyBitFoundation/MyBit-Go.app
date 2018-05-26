e5e192e1ced6c9bc1b667c5a021a8f9d6736d32a
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-26 16:14:25 +0100

    Added unified loading reducer and routing to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 
 import assets from './assets';
 import errors from './errors';
+import loading from './loading';
 import misc from './misc';
 import portfolio from './portfolio';
 import user from './user';
@@ -10,10 +12,12 @@ import transactions from './transactions';
 const rootReducer = combineReducers({
   assets,
   errors,
+  loading,
   misc,
   portfolio,
-  user,
+  router: routerReducer,
   transactions,
+  user,
 });
 
 export default rootReducer;
