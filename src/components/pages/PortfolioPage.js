65b57df6eb1d9e4de68acb1e87e9eb5ea8cbe55b
Kamuela Franco <kamuela.franco@gmail.com>
2018-05-31 22:18:58 +0100

    Proper display of portfolio assets

--- src/components/pages/PortfolioPage.js
+++ src/components/pages/PortfolioPage.js
@@ -5,17 +5,57 @@ import LoadingPage from './LoadingPage';
 import TotalPortfolioValue from '../TotalPortfolioValue';
 import TotalPortfolioRevenue from '../TotalPortfolioRevenue';
 
+const fromWeiToEth = weiValue => window.web3.utils.fromWei(weiValue, 'ether');
+
+const getOwnedAssets = assets => assets.filter(asset => asset.ownershipUnits > 0);
+
+const getPortfolioValue =
+  (assets, currentEthPrice) =>
+    assets.reduce((accumulator, currentValue) =>
+      accumulator + (fromWeiToEth(currentValue.ownershipUnits, 'ether') * currentEthPrice), 0);
+
+const getPortfolioRevenue =
+  (assets, currentEthPrice) =>
+    assets.reduce((accumulator, currentValue) =>
+      accumulator + (Number(currentValue.assetIncome) * currentEthPrice), 0);
+
+const getPortfolioValueAssets = (assets, currentEthPrice) => assets.map(asset => ({
+  assetID: asset.assetID,
+  name: asset.assetID,
+  ownership: asset.ownershipUnits,
+  value: String(fromWeiToEth(asset.ownershipUnits) * currentEthPrice),
+}));
+
+const getPortfolioRevenueAssets = (assets) => assets.map(asset => ({
+  assetID: asset.assetID,
+  name: asset.assetID,
+  monthlyRevenue: String(Number(asset.assetIncome) / 12), // TODO: This isn't the real calculation
+  totalRevenue: asset.assetIncome,
+}));
+
 const PortfolioPage = ({ state }) => {
   if (state.loading.portfolio) {
     return <LoadingPage message="Loading portfolio" />;
   }
 
+  const ownedAssets = getOwnedAssets(state.assets);
+  const totalPortfolioValue = getPortfolioValue(ownedAssets, state.misc.currentEthInUsd);
+  const totalPortfolioRevenue = getPortfolioRevenue(ownedAssets, state.misc.currentEthInUsd);
+  const portfolioValueAssets = getPortfolioValueAssets(ownedAssets);
+  const portfolioRevenueAssets = getPortfolioRevenueAssets(ownedAssets);
+
   return (
     <div>
       <div className="Portfolio">
         <div className="Portfolio__wrapper">
-          <TotalPortfolioValue totalPortfolioValue="100" assets={state.assets} />
-          <TotalPortfolioRevenue totalPortfolioRevenue="100" assets={state.assets} />
+          <TotalPortfolioValue
+            totalPortfolioValue={String(totalPortfolioValue)}
+            portfolioValueAssets={portfolioValueAssets}
+          />
+          <TotalPortfolioRevenue
+            totalPortfolioRevenue={String(totalPortfolioRevenue)}
+            portfolioRevenueAssets={portfolioRevenueAssets}
+          />
         </div>
       </div>
     </div>
