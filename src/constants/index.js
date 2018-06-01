c727f76cc58d7752269663d575b4f9724fa12fe0
Kamuela Franco <kamuela.franco@gmail.com>
2018-06-01 14:37:34 +0100

    Fixed development case

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const ARTIFICIAL_DELAY_IN_MS = 3000;
 export const USD_MYB_SYMBOL = 'USD/MYB';
 export const noop = () => {};
-export const debug = process.env.NODE_ENV === 'DEVELOPMENT' ? console.log : noop;
+export const debug = process.env.NODE_ENV === 'development' ? console.log : noop;
 export const MYBIT_TICKER_COINMARKETCAP = 1902;
 export const ETHEREUM_TICKER_COINMARKETCAP = 1027;
 // TODO: Needs to be implemented server-side, API_KEY needs to be created
