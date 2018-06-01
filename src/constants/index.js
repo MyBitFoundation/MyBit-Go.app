115a5dcad716953314156d2bd1ead69a538110ab
Kamuela Franco <kamuela.franco@gmail.com>
2018-06-01 13:44:24 +0100

    Updated TODO

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const noop = () => {};
 export const debug = process.env.NODE_ENV === 'DEVELOPMENT' ? console.log : noop;
 export const MYBIT_TICKER_COINMARKETCAP = 1902;
 export const ETHEREUM_TICKER_COINMARKETCAP = 1027;
-// TODO: Needs to be implemented server-side
+// TODO: Needs to be implemented server-side, API_KEY needs to be created
 export const ETHERSCAN_API_KEY = '';
 export const ETHERSCAN_TX_BY_ADDR_ENDPOINT =
   (apiKey, address) =>
