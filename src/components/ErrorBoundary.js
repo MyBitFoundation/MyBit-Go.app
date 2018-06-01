02807e5ca777025b81b0efeed451af094111d076
Kamuela Franco <kamuela.franco@gmail.com>
2018-06-01 14:19:19 +0100

    Added an ErrorBoundary component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { debug } from '../constants';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    debug(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.component()).isRequired,
+};
+
+export default ErrorBoundary;
