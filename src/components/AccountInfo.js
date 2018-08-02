779f2d4fb77556bf032a4bdb457160177670aef0
Branislav <brani.djuric@gmail.com>
2018-08-02 14:42:29 +0200

    MYB token correct math representation;

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Loading } from 'carbon-components-react';
+import Web3 from 'web3';
+
 import Address from './Address';
 import '../styles/AccountInfo.css';
 
-const AccountInfo = ({ myBitBalance, ethBalance, userName }) => (
-  <div className="AccountInfo">
+const AccountInfo = ({ myBitBalance, ethBalance, userName }) => {
+
+  if(myBitBalance !== 0 || myBitBalance !== 'undefined') {
+    let myBitBalanceBN = Web3.utils.toBN(myBitBalance);
+    myBitBalance = Web3.utils.fromWei(myBitBalanceBN, 'ether');
+  }
+  return(
+    <div className="AccountInfo">
     <div className="AccountInfo__balance">
       <b className="AccountInfo__balance-header">Balance</b>
-      {!ethBalance ? (
+      {!ethBalance || !myBitBalance ? (
         <Loading
           className="AccountInfo__balance--is-loading"
           small
@@ -23,7 +31,8 @@ const AccountInfo = ({ myBitBalance, ethBalance, userName }) => (
     </div>
     <Address className="AccountInfo__address" userName={userName} />
   </div>
-);
+  )
+}
 
 AccountInfo.defaultProps = {
   myBitBalance: '',
