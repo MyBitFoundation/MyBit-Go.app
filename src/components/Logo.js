39070f74817138a33a74df1e2c986d083576a0e6
Branislav <brani.djuric@gmail.com>
2018-09-13 22:08:36 +0200

    mybit go logo (svg)

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import logo from '../images/mybit-logo-white.svg';
+import logo from '../images/my-bit-go.svg';
 
 const Logo = ({ className }) => (
   <img alt="MyBit Logo" className={className} src={logo} />
