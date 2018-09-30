d9f19a23facae44185bc0e4eca151537ed0b401c
Branislav <brani.djuric@gmail.com>
2018-09-30 22:18:41 +0200

    antd modal for mm_check
    Asset Location

--- src/components/MetamaskBooting.js
+++ src/components/MetamaskBooting.js
@@ -6,48 +6,53 @@ import { Modal, Button } from 'antd';
 import '../styles/MetamaskAudit.css';
 import MetamaskLogo from '../images/metamask.svg';
 
-const MetamaskBooting = ({ isBraveBrowser, extensionUrl }) => (
-  <Modal className="MetamaskAudit" open passiveModal>
-    <p className="MetamaskAudit__title">
-      {isBraveBrowser ? (
-        <p>
-          To start investing, please <br /> install Metamask.
-        </p>
-      ) : (
-        <p>
-          To start investing, please <br /> connect your wallet.
-        </p>
+const MetamaskBooting = ({ isBraveBrowser, extensionUrl }) => {
+  const visible = true;
+  return (
+    <Modal className="MetamaskAudit" visible={visible}>
+      <p className="MetamaskAudit__title">
+        {isBraveBrowser ? (
+          <p>
+            To start investing, please <br /> install Metamask.
+          </p>
+        ) : (
+          <p>
+            To start investing, please <br /> connect your wallet.
+          </p>
+        )}
+      </p>
+      <MetamaskLogo
+        className="MetamaskAudit__metamaskfox-image"
+      />
+      {!isBraveBrowser && (
+        <div>
+          <a href={extensionUrl} target="_blank" rel="noopener noreferrer">
+            <Button
+              small
+              kind="primary"
+              className="MetamaskAudit__metamaskinstall-button"
+            >
+              Install Metamask and Refresh
+            </Button>
+          </a>
+          <br />
+        </div>
       )}
-    </p>
-    <MetamaskLogo
-      className="MetamaskAudit__metamaskfox-image"
-    />
-    {!isBraveBrowser && (
-      <div>
-        <a href={extensionUrl} target="_blank" rel="noopener noreferrer">
-          <Button
-            small
-            kind="primary"
-            className="MetamaskAudit__metamaskinstall-button"
-          >
-            Install Metamask and Refresh
-          </Button>
-        </a>
-        <br />
-      </div>
-    )}
-    <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">
-      <Button
-        small
-        kind="secondary"
-        className="MetamaskAudit__metamaskmanual-button"
-      >
-        What is Metamask?
-      </Button>
-    </a>
-    <br />
-  </Modal>
-);
+      <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">
+        <Button
+          small
+          kind="secondary"
+          className="MetamaskAudit__metamaskmanual-button"
+        >
+          What is Metamask?
+        </Button>
+      </a>
+      <br />
+    </Modal>
+  );
+}
+
+
 
 MetamaskBooting.propTypes = {
   isBraveBrowser: PropTypes.bool.isRequired,
