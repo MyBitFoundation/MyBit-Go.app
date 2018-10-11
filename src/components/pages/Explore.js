5365717c36db082573757c3a88c6e8d606a4780c
Branislav <brani.djuric@gmail.com>
2018-10-11 22:12:27 +0200

    function filter, static; styled components as dependency;

--- src/components/pages/Explore.js
+++ src/components/pages/Explore.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
 // import { Link } from 'react-router-dom';
 import Button from 'antd/lib/button';
 import 'antd/lib/button/style';
 import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
 import { Switch, Icon } from 'antd';
 import 'antd/lib/switch/style';
-// import 'antd/lib/row/style';
+import 'antd/lib/row/style';
+import 'antd/lib/col/style';
 
 import Asset from '../Asset';
 // import NotFoundPage from './NotFoundPage';
@@ -14,18 +17,49 @@ import LoadingPage from './LoadingPage';
 
 const Explore = ({ loading, assets }) => {
 
-  const categories = ['Crypto', 'Real Estate', 'Energy', 'Machinery', 'Transpotation', 'Other'];
+  // fundingStage 
+  // = 1 open for funding
+  // = 2 failed
+  // = 3 success but needs confirmation, payout() needs to be called on it
+  // = 4 live
+
+  // new categories 
+  // Crypto', 'Real Estate', 'Energy', 'Machinery', 'Transpotation'
+  let filter = {
+    fundingStage: '4', 
+    category: 'other',
+    category: 'solarenergy'
+  };
+
+  assets = assets.filter(function(item) {
+    for (var key in filter) {
+      if (item[key] === undefined || item[key] != filter[key])
+        return false;
+    }
+    return true;
+  });
+
   const loadingElement = (
-    <LoadingPage message="Loading assets" hasBackButton />
+    <LoadingPage message="Loading assets" />
   );
   const filterButtons = (
     <div>
       <Button>Crypto</Button>
+      {/* <ToogleButton active=""> </ToggleButton> */}
       <Button>Real Estate</Button>
       <Button>Energy</Button>
       <Button>Machinery</Button>
     </div>
   );
+  
+  const FilterCategoryContainer = styled.div`
+    margin-left: 20px;
+  `
+  const FilterFundingActiveContainer = styled.div`
+    float: right;
+  `
+
+
   const switchActive = (
     <div>
       <label>Funding Active</label>
@@ -33,8 +67,8 @@ const Explore = ({ loading, assets }) => {
     </div>
   );
   const assetsToRender = [
-    filterButtons,
-    switchActive,
+    // filterButtons,
+    // switchActive,
     <div>
       {assets.map(asset => (
         <Asset
@@ -66,8 +100,15 @@ const Explore = ({ loading, assets }) => {
 
   return (
     <div>
-      {toRender}
-    </div>
+      <FilterCategoryContainer>
+        {filterButtons} 
+      </FilterCategoryContainer>
+      <FilterFundingActiveContainer>
+        {switchActive}
+      </FilterFundingActiveContainer>
+        
+        {toRender} 
+    </div> 
   )
 };
 
