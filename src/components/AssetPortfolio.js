8c52ed4ed68eebde6144579fe3008ee516302e88
Branislav <brani.djuric@gmail.com>
2018-10-18 17:21:24 +0200

    Portfolio Page visual details

--- src/components/AssetPortfolio.js
+++ src/components/AssetPortfolio.js
@@ -46,7 +46,7 @@ const AssetPortfolio = ({
   );
 
   return (
-    <Col xs={24} sm={24} md={12} lg={12} xl={8} style={{ padding: '20px 20px' }}>
+    <Col xs={24} sm={24} md={12} lg={12} xl={8} style={{ padding: '20px 40px 10px 0px' }}>
       <div className="AssetPortfolio">
         <div
           className="AssetPortfolio__image"
