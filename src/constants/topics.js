bece50dcc1e74be4b32bb36942c6a9293641768a
Valeriy Tverdohleb <v@tverdohleb.com>
2018-10-25 17:11:59 +0300

    add another topics for funding events

--- src/constants/topics.js
+++ src/constants/topics.js
@@ -3,14 +3,8 @@ export const fundingTopics = {
   LogNewFunder: '0xa1e9db5829418205d34d8823e5d72d89769c56512622ab5acf29b9f2554a13f0',
   LogAssetFunded: '0xdfae09646add0c3ff3c8a61988fda3ec21616837b9cf280a54385d3da0997355',
   LogAssetFundingFailed: '0xa946ebac75e7958f76eca23dda8528aec8e27e72f7a709f94b3777c3bcd9de4c',
-  // 'LogAssetFundingSuccess': '',
-  // 'LogRefund': '',
-  // 'LogAssetPayout': '',
-  // 'LogDestruction': ''
-
-  //  unknown topicks
-  // '0x7b6ccf85690b8ce1b7d21a94ca738803a9da7dc74e10140f269efa0d8d6fb851',
-  // '0x8c7ae8776186c2e87b266886e8af282b7fdb52b47993376583987a14656a3678',
-  // '0xb91cc48e5aadfdc16d855301e83a9a7088f6ba3850a8f3bdee70f4af7c3825df',
-  // '0xca2bea473aeb046f60d64c3377f67f3648239971bf752a2515649ff970dd13ef',
+  LogAssetFundingSuccess: '0xca2bea473aeb046f60d64c3377f67f3648239971bf752a2515649ff970dd13ef',
+  LogRefund: '0x7b6ccf85690b8ce1b7d21a94ca738803a9da7dc74e10140f269efa0d8d6fb851',
+  LogAssetPayout: '0xb91cc48e5aadfdc16d855301e83a9a7088f6ba3850a8f3bdee70f4af7c3825df',
+  LogDestruction: '0x8c7ae8776186c2e87b266886e8af282b7fdb52b47993376583987a14656a3678',
 };
