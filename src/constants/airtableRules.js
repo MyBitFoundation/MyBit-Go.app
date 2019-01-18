cf36c596d8240c91ca417f12e2871fa45ed8fd40
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2019-01-18 18:04:27 +0000

    removed unneded index

--- src/constants/airtableRules.js
+++ src/constants/airtableRules.js
@@ -18,7 +18,7 @@ export const AIRTABLE_CATEGORIES_RULES = [
 // make sure the data from airtable is correct
 // and that every required field is filled
 export const verifyDataAirtable = (rules, records) => {
-  return records.filter(({ fields }, index) =>
+  return records.filter(({ fields }) =>
     rules.every(field => {
       const valueOfField = fields[field];
       return Object.keys(fields).includes(field) && valueOfField;
