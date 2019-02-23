f733af5ede274e381abc65d52552a4ea4032d825
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2019-02-23 14:10:55 +0000

    removed back button and padding in the asset page

--- components/AssetDetails/styledAssetDetailsLeftCol.js
+++ components/AssetDetails/styledAssetDetailsLeftCol.js
@@ -4,9 +4,11 @@ import {
 } from 'antd';
 
 const StyledAssetDetailsLeftCol = styled(Col)`
-  padding: 20px;
-  background-color: #f9f9f9;
-  box-shadow: 0 4px 12px 0 rgba(0,0,0,.1);
+  ${({theme}) => theme.tablet`
+    padding: 20px;
+    background-color: #f9f9f9;
+    box-shadow: 0 4px 12px 0 rgba(0,0,0,.1);
+  `}
 }`
 
 export default StyledAssetDetailsLeftCol;
--- pages/asset/styledButton.js
+++ pages/asset/styledButton.js
@@ -4,7 +4,12 @@ import {
 } from 'antd';
 
 const StyledButton = styled(Button)`
+  display: none;
   margin-bottom: 40px;
+
+  ${({theme}) => theme.tablet`
+    display: block;
+  `}
 }`
 
 export default StyledButton;
