4870344f713a41034a68d7438e1d23731a731e18
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2019-03-22 00:51:56 +0000

    re-added files with the correct names

---
+++ components/AssetDetails/assetDetailsRightCol.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import {
+  Col,
+} from 'antd';
+
+const AssetDetailsRighCol = styled(Col)`
+  margin-top: 40px;
+
+  @media(min-width: 991px){
+    padding-left: 10px;
+    margin-top: 0px;
+  }
+}`
+
+export default AssetDetailsRighCol;
