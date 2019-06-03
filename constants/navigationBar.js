9a09428f6f5cff58b64dee0d957988aaa4abaea1
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2019-06-03 14:45:16 +0100

    added asset managers URL to the navigation

--- constants/navigationBar.js
+++ constants/navigationBar.js
@@ -5,6 +5,7 @@ import TransactionsIcon from '../static/history.svg';
 import ListAssetIcon from '../static/plus.svg';
 import KnowledgeBaseIcon from '../static/question.svg';
 import WatchIcon from '../static/watchList.svg';
+import AssetManagers from '../static/asset-managers.svg';
 
 export const navbarOptions = (currentPath) => [
   {
@@ -32,6 +33,11 @@ export const navbarOptions = (currentPath) => [
     icon: ListAssetIcon,
     selected: currentPath === '/list-asset',
     url: '/list-asset',
+  }, {
+    name: 'Asset Managers',
+    icon: AssetManagers,
+    selected: currentPath === '/asset-managers',
+    url: '/asset-managers',
   }, {
     name: 'Knowledge Base',
     icon: KnowledgeBaseIcon,
