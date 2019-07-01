e14382846faa284275b764952c875fefe62b47ab
csmartinsfct <cristiano.santos.martinss@gmail.com>
2019-07-01 13:32:59 +0100

    adds Payout and Acquired types to the asset details page and fixes mobile

--- components/UI/AssetFundingDetails/index.js
+++ components/UI/AssetFundingDetails/index.js
@@ -1,42 +1,47 @@
+import styled, { css } from 'styled-components';
 import { memo } from 'react';
-import FundingDetailsWrapper from './fundingDetailsWrapper';
-import FundingDetailsSection from './fundingDetailsSection';
-import FundingDetailsTitle from './fundingDetailsTitle';
-import FundingDetailsValue from './fundingDetailsValue';
+import Item from './Item';
+
+const FundingDetailsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto auto;
+  grid-row-gap: 20px;
+  ${({theme}) => theme.mobileL`
+    grid-template-columns: auto auto auto;
+  `}
+`
 
 const AssetFundingDetails = memo(({
   fundingGoal,
   fundingProgress,
   funded,
   numberOfInvestors,
+  cryptoPayout,
+  cryptoPurchase,
 }) => (
   <FundingDetailsWrapper>
-    <FundingDetailsSection>
-      <FundingDetailsTitle>
-        Funds raised
-      </FundingDetailsTitle>
-      <FundingDetailsValue
-        funded={funded}
-      >
-        {fundingProgress}
-      </FundingDetailsValue>
-    </FundingDetailsSection>
-    <FundingDetailsSection>
-      <FundingDetailsTitle>
-        Funding goal
-      </FundingDetailsTitle>
-      <FundingDetailsValue>
-        {fundingGoal}
-      </FundingDetailsValue>
-    </FundingDetailsSection>
-    <FundingDetailsSection>
-      <FundingDetailsTitle>
-        Investors
-      </FundingDetailsTitle>
-      <FundingDetailsValue>
-        {numberOfInvestors}
-      </FundingDetailsValue>
-    </FundingDetailsSection>
+    <Item
+      funded={funded}
+      title="Funds raised"
+      value={fundingProgress}
+    />
+    <Item
+      title="Funding goal"
+      value={fundingGoal}
+    />
+    <Item
+      title="Investors"
+      value={numberOfInvestors}
+    />
+    <Item
+      title="Payout"
+      value={cryptoPayout ? 'Crypto' : 'Fiat'}
+    />
+    <Item
+      title="Acquired"
+      value={cryptoPurchase ? 'Via Crypto' : 'Via Fiat'}
+    />
   </FundingDetailsWrapper>
 ))
 
