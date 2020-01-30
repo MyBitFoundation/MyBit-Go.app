f4716ed4f9f124a762dfcecc8b7baa65ba53d154
Tony Crowe <github@tonycrowe.com>
2020-01-30 16:42:44 -0700

    fix infrastructure

--- next.config.js
+++ next.config.js
@@ -1,58 +1,17 @@
-/* eslint-disable */
-const path = require('path')
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-const withCss = require('@zeit/next-css')
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
 
-// fix: prevents error when .css files are required by node
-if (typeof require !== 'undefined') {
-  require.extensions['.css'] = file => {}
-}
+const os = require("os");
+const path = require("path");
+const bundleAnalyzer = require('@next/bundle-analyzer');
+const withCss = require('@zeit/next-css');
+const { ANALYZE, GOOGLE_PLACES_API_KEY } = process.env;
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: ANALYZE === true || ANALYZE === "true",
+  openAnalyzer: false
+});
 
 module.exports = withBundleAnalyzer(withCss({
-  webpack: (config, { dev, isServer }) => {
-    if(!dev) {
-      config.devtool = false
-
-       // disable soucemaps of babel-loader
-      for (const r of config.module.rules) {
-        if (r.loader === 'babel-loader') {
-          r.options.sourceMaps = false
-        }
-      }
-    }
-
-    if (!isServer) {
-      const cacheGroups = config.optimization.splitChunks.cacheGroups
-      delete cacheGroups.react
-      cacheGroups.default = false
-      cacheGroups.vendors = {
-        name: 'vendors',
-        test: /[\\/](node_modules|packages)[\\/]/,
-        enforce: true,
-        priority: 20,
-      }
-      cacheGroups.commons = {
-        name: 'commons',
-        minChunks: 2,
-        priority: 10,
-      }
-    }
-
-    return config
-  },
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html'
-    },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/client.html'
-    }
-  },
-  publicRuntimeConfig: {
-    GOOGLE_PLACES_API_KEY: process.NODE_ENV === 'production' && process.env.GOOGLE_PLACES_API_KEY,
-  }
+  distDir: 'build',
+  env: { GOOGLE_PLACES_API_KEY }
 }));
