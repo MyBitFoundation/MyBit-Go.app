293c0d3837ea96985aae45c101583f87d1eb38d7
Tony Crowe <github@tonycrowe.com>
2020-02-29 14:38:25 -0700

    remove airtable 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import App, { Container } from "next/app";
-import AirtableProvider, {
-  withAirtableContext
-} from "components/AirtableContext";
 import BlockchainProvider from "components/BlockchainContext";
 import KyberProvider from "components/KyberContext";
 import AssetsProvider from "components/AssetsContext";
@@ -124,27 +121,25 @@ const WithProviders = ({
   userHasMetamask
 }) => (
   <NotificationsProvider>
-    <AirtableProvider network={network} userHasMetamask={userHasMetamask}>
-      <KyberProvider
-        network={network}
+    <KyberProvider
+      network={network}
+      supportedNetworks={SUPPORTED_NETWORKS}
+      userHasMetamask={userHasMetamask}
+    >
+      <MetamaskProvider
         supportedNetworks={SUPPORTED_NETWORKS}
-        userHasMetamask={userHasMetamask}
+        setNetwork={setNetwork}
+        setUserHasMetamask={setUserHasMetamask}
       >
-        <MetamaskProvider
-          supportedNetworks={SUPPORTED_NETWORKS}
-          setNetwork={setNetwork}
-          setUserHasMetamask={setUserHasMetamask}
-        >
-          <AssetsProvider>
-            <BlockchainProvider supportedNetworks={SUPPORTED_NETWORKS}>
-              <ThreeBoxProvider>
-                <TermsOfServiceProvider>{children}</TermsOfServiceProvider>
-              </ThreeBoxProvider>
-            </BlockchainProvider>
-          </AssetsProvider>
-        </MetamaskProvider>
-      </KyberProvider>
-    </AirtableProvider>
+        <AssetsProvider>
+          <BlockchainProvider supportedNetworks={SUPPORTED_NETWORKS}>
+            <ThreeBoxProvider>
+              <TermsOfServiceProvider>{children}</TermsOfServiceProvider>
+            </ThreeBoxProvider>
+          </BlockchainProvider>
+        </AssetsProvider>
+      </MetamaskProvider>
+    </KyberProvider>
   </NotificationsProvider>
 );
 
