8391035881780594f36b957664cde4c536d4e15b
Bhanu Teja P <pbteja1998@gmail.com>
2020-06-05 10:01:44 +0530

    Add etherscan-api-key as env variable

--- constants/links.js
+++ constants/links.js
@@ -22,7 +22,7 @@ export const ExternalLinks = {
   METAMASK_CHROME: 'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
   METAMASK_OPERA: 'https://addons.opera.com/extensions/details/metamask/',
   ETHERSCAN_TX_BY_ADDR_ENDPOINT: address =>
-    `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc`,
+    `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`,
   ETHERSCAN_TX: txHash =>
     `https://api-ropsten.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${txHash}`,
   kyberSwapRopsten: (from, to) => `https://ropsten.kyber.network/swap/${from}-${to}`,
