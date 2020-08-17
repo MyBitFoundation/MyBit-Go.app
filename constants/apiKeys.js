da68c4dbfa5d554efa79f6dc3e4575f8e6aaf404
Roy Xu <boyuanxu9@gmail.com>
2020-08-17 10:05:00 -0400

    Use Etherscan API keys due to the updated rule.

---
+++ constants/apiKeys.js
@@ -0,0 +1,1 @@
+export const ETHERSCAN_API_KEY = process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY;
--- constants/links.js
+++ constants/links.js
@@ -1,3 +1,5 @@
+import { ETHERSCAN_API_KEY } from './apiKeys'
+
 const baseUrl = 'http://localhost'
 const port = 8081;
 const secondPort = 8082;
@@ -22,9 +24,9 @@ export const ExternalLinks = {
   METAMASK_CHROME: 'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
   METAMASK_OPERA: 'https://addons.opera.com/extensions/details/metamask/',
   ETHERSCAN_TX_BY_ADDR_ENDPOINT: address =>
-    `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc`,
+    `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc&apiKey=${ETHERSCAN_API_KEY}`,
   ETHERSCAN_TX: txHash =>
-    `https://api-ropsten.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${txHash}`,
+    `https://api-ropsten.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${txHash}&apiKey=${ETHERSCAN_API_KEY}`,
   kyberSwapRopsten: (from, to) => `https://ropsten.kyber.network/swap/${from}-${to}`,
   getEtherscanAddressURL: (network, address) => `https://${network === 'ropsten' ? 'ropsten.' : ''}etherscan.io/address/${address}`
 }
