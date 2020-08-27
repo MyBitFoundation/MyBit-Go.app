3903d949a36f65552a1e20589443b5017213bad6
Boyuan Xu <boyuanxu9@gmail.com>
2020-08-27 08:41:38 -0400

    Update platform fees

--- constants/platformFees.js
+++ constants/platformFees.js
@@ -1,3 +1,3 @@
-export const MYBIT_FOUNDATION_SHARE = 0.01;
-export const MYBIT_FOUNDATION_FEE = 0.03;
+export const MYBIT_FOUNDATION_SHARE = 0.05;
+export const MYBIT_FOUNDATION_FEE = 0.05;
 export const FIAT_TO_CRYPTO_CONVERSION_FEE = 0.08;
