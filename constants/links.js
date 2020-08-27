854e7d20adf1154853f6b4538371f05e4f94373c
Boyuan Xu <boyuanxu9@gmail.com>
2020-08-27 13:38:21 -0400

    Update upload links

--- constants/links.js
+++ constants/links.js
@@ -1,31 +1,29 @@
-import { ETHERSCAN_API_KEY } from './apiKeys'
+import { ETHERSCAN_API_KEY } from './apiKeys';
 
-const baseUrl = 'http://localhost'
+const baseUrl = 'http://localhost';
 const port = 8081;
 const secondPort = 8082;
-const host = `${baseUrl}:${port}`
-const secondHost = `${baseUrl}:${secondPort}`
+const host = `${baseUrl}:${port}`;
+const secondHost = `${baseUrl}:${secondPort}`;
 const apiEndpoint = 'https://api.mybit.io';
 
 export const InternalLinks = {
   S3: 'https://s3.eu-central-1.amazonaws.com/mybit-go/',
-  getAirtableAssetListings: network => process.env.NODE_ENV === 'development' ? `${host}/api/airtable/assetListings/${network}` : `/api/airtable/assetListings/${network}`,
-  getAirtableOperators: network => process.env.NODE_ENV === 'development' ? `${host}/api/airtable/operators/${network}` : `/api/airtable/operators/${network}`,
-  updateAirtableAssets: network => process.env.NODE_ENV === 'development' ? `${host}/api/airtable/update/${network}` : `/api/airtable/update/${network}`,
-  updateAirtableAssetListing: network => process.env.NODE_ENV === 'development' ? `${host}/api/airtable/updateAssetListing/${network}` : `/api/airtable/updateAssetListing/${network}`,
-  S3_UPLOAD: process.env.NODE_ENV === 'development' ? `${host}/api/files/upload/ropsten` : '/api/files/upload',
+  getAirtableAssetListings: network => (process.env.NODE_ENV === 'development' ? `${host}/api/airtable/assetListings/${network}` : `/api/airtable/assetListings/${network}`),
+  getAirtableOperators: network => (process.env.NODE_ENV === 'development' ? `${host}/api/airtable/operators/${network}` : `/api/airtable/operators/${network}`),
+  updateAirtableAssets: network => (process.env.NODE_ENV === 'development' ? `${host}/api/airtable/update/${network}` : `/api/airtable/update/${network}`),
+  updateAirtableAssetListing: network => (process.env.NODE_ENV === 'development' ? `${host}/api/airtable/updateAssetListing/${network}` : `/api/airtable/updateAssetListing/${network}`),
+  S3_UPLOAD: process.env.NODE_ENV === 'development' ? `${host}/api/files/upload` : '/api/files/upload',
   S3_ASSET_FILES: process.env.NODE_ENV === 'development' ? `${host}/api/assets/files` : '/api/assets/files',
   GAS_PRICE: `${apiEndpoint}/gasprice`,
-}
+};
 
 export const ExternalLinks = {
   METAMASK_FIREFOX: 'https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/',
   METAMASK_CHROME: 'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
   METAMASK_OPERA: 'https://addons.opera.com/extensions/details/metamask/',
-  ETHERSCAN_TX_BY_ADDR_ENDPOINT: address =>
-    `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc&apiKey=${ETHERSCAN_API_KEY}`,
-  ETHERSCAN_TX: txHash =>
-    `https://api-ropsten.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${txHash}&apiKey=${ETHERSCAN_API_KEY}`,
+  ETHERSCAN_TX_BY_ADDR_ENDPOINT: address => `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc&apiKey=${ETHERSCAN_API_KEY}`,
+  ETHERSCAN_TX: txHash => `https://api-ropsten.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${txHash}&apiKey=${ETHERSCAN_API_KEY}`,
   kyberSwapRopsten: (from, to) => `https://ropsten.kyber.network/swap/${from}-${to}`,
-  getEtherscanAddressURL: (network, address) => `https://${network === 'ropsten' ? 'ropsten.' : ''}etherscan.io/address/${address}`
-}
+  getEtherscanAddressURL: (network, address) => `https://${network === 'ropsten' ? 'ropsten.' : ''}etherscan.io/address/${address}`,
+};
